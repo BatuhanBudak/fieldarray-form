0b4051ed5afc51a837211df971a36366cf9555d3
Batuhan Budak <batuhan.budak@gmail.com>
2022-12-21 18:56:45 +0300

    Add styling to FieldArray component using materialUI

--- src/components/FieldArray.tsx
+++ src/components/FieldArray.tsx
@@ -1,5 +1,17 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Typography,
+  TextField,
+  InputAdornment,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 import React from "react";
-import { useForm, useFieldArray } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 
 const emptyDonation = { institution: "", percentage: 0 };
 
@@ -14,7 +26,7 @@ const FieldArray = () => {
   const {
     register,
     control,
-    formState: { isSubmitted, isSubmitting },
+    formState: { isSubmitted, isSubmitting, errors },
   } = useForm<FormValues>({
     defaultValues: {
       fullName: "",
@@ -30,36 +42,147 @@ const FieldArray = () => {
   });
 
   return (
-    <form>
-      <input {...register("fullName")} />
-      <input
-        type={"number"}
-        {...(register("donationAmount"), { valueAsNumber: true })}
-      />
-      <h2>All Donations</h2>
-      {fields.map((field, i) => (
-        <div key={field.id}>
-          <section key={field.id}>
-            <input {...register(`donations.${i}.institution` as const)} />
-            <input {...register(`donations.${i}.percentage` as const)} />
-            <button
-              disabled={isSubmitted || isSubmitting}
-              onClick={() => remove(i)}
-            >
-              Delete
-            </button>
-          </section>
-        </div>
-      ))}
+    <Card>
+      <CardContent>
+        <form>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <Controller
+                name="fullName"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Full name"
+                    variant="outlined"
+                    fullWidth
+                    margin="dense"
+                    error={!!errors.fullName}
+                    helperText={errors.fullName?.message ?? ""}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item>
+              <Controller
+                name="donationAmount"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">$</InputAdornment>
+                      ),
+                    }}
+                    label="Donation amount"
+                    variant="outlined"
+                    fullWidth
+                    margin="dense"
+                    error={!!errors.donationAmount}
+                    helperText={errors.donationAmount?.message ?? ""}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="body2">All Donations</Typography>
+            </Grid>
+            {fields.map((field, i) => (
+              <Grid
+                container
+                item
+                key={field.id}
+                spacing={2}
+                alignItems="center"
+              >
+                <Grid container item spacing={2} xs={12} sm={11} key={field.id}>
+                  <Grid item xs={12} sm={6}>
+                    <Controller
+                      name={`donations.${i}.institution` as const}
+                      control={control}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          label="Institution"
+                          fullWidth
+                          error={!!errors.donations?.root?.message}
+                          helperText={errors.donations?.root?.message ?? ""}
+                        />
+                      )}
+                    />
+                  </Grid>
 
-      <button type="button" onClick={() => append(emptyDonation)}>
-        Add donation:
-      </button>
-      <input type="checkbox" />
-      <button>
-        {isSubmitting ? "Submitting" : isSubmitted ? "Submited" : "Submit"}
-      </button>
-    </form>
+                  <Grid item xs={12} sm={6}>
+                    <Controller
+                      name={`donations.${i}.percentage` as const}
+                      control={control}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          label="Percentage (%)"
+                          fullWidth
+                          error={!!errors.donations?.root?.message}
+                          helperText={errors.donations?.root?.message ?? ""}
+                        />
+                      )}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} sm="auto">
+                  <Button
+                    disabled={isSubmitted || isSubmitting}
+                    onClick={() => remove(i)}
+                  >
+                    Delete
+                  </Button>
+                </Grid>
+              </Grid>
+            ))}
+
+            <Grid item>
+              <Button
+                type="button"
+                variant="contained"
+                onClick={() => append(emptyDonation)}
+              >
+                Add donation
+              </Button>
+            </Grid>
+            <Grid item>
+              <Controller
+                name="termsAndConditions"
+                control={control}
+                render={({ field }) => (
+                  <FormControlLabel
+                    {...field}
+                    control={<Checkbox />}
+                    label="I accept terms and conditions"
+                  />
+                )}
+              ></Controller>
+            </Grid>
+            <Grid item>
+              <Button
+                disabled={isSubmitting}
+                type="submit"
+                variant="contained"
+                color="primary"
+                startIcon={
+                  isSubmitting ? <CircularProgress size="0.9rem" /> : undefined
+                }
+              >
+                {isSubmitting
+                  ? "Submitting"
+                  : isSubmitted
+                  ? "Submited"
+                  : "Submit"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 export default FieldArray;
